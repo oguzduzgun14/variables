3e193282dff18e637f29ab6a65ba425c71f6437f
oguzduzgun14 <oguzduzgun1414@gmail.com>
2021-09-28 16:11:48 +0300

    Initial commit

---
+++ src/variables/Main.java
@@ -0,0 +1,26 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String studentName="veli";
+		int studentAge=23;
+		System.out.println(studentName);
+		studentName="ali";
+		System.out.println(studentName+" nin yasý "+studentAge);
+		String studentNationalIdentity="1652612131615";
+		System.out.println("TC Kimlik Numarasý :"+studentNationalIdentity);
+		double studentLength=1.75;
+		System.out.println("Öðrencinin Boyu :"+studentLength);
+		boolean studentIsActive=false;//true yada false
+		System.out.println("Öðrenci Devam :"+studentIsActive);
+		
+			
+		
+		
+		
+
+	}
+
+}
